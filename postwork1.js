cf4103c1e2b5a8ba1eb6501ee750a85a52dea219
cafaggi <carlos.cafaggi@gmail.com>
2021-07-28 22:19:49 -0500

    fix tests

--- postwork1.js
+++ postwork1.js
@@ -18,6 +18,11 @@ function deepEqual(obj1, obj2) {
     return true;
 }
 
+const john = {
+    firstName: 'John',
+    lastName: 'Doe'
+   }
+   
 console.log('Test 1:', deepEqual(1, 1)) // true
 console.log('Test 2:', deepEqual(1, '1')) // false
 console.log('Test 3:', deepEqual(john, john)) // true
