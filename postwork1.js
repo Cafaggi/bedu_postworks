b90a9d3d2c6287fe4681eb3ac1a45ffc3c402be5
cafaggi <carlos.cafaggi@gmail.com>
2021-07-29 19:45:19 -0500

    add spaces

--- postwork1.js
+++ postwork1.js
@@ -22,7 +22,10 @@ const john = {
     firstName: 'John',
     lastName: 'Doe'
    }
+
+
    
+
 console.log('Test 1:', deepEqual(1, 1)) // true
 console.log('Test 2:', deepEqual(1, '1')) // false
 console.log('Test 3:', deepEqual(john, john)) // true
