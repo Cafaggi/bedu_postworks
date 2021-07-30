30c3ab87a27f35c52178f88921c133cfa2530fe7
cafaggi <carlos.cafaggi@gmail.com>
2021-07-29 22:00:21 -0500

    make the frequency function work

--- postwork1.js
+++ postwork1.js
@@ -57,3 +57,25 @@ function chunk(array, size) {
   console.log('Test 1:', chunk(data, 1)) // [[1], [2], [3], [4], [5], [6], [7], [8]]
   console.log('Test 2:', chunk(data, 2)) // [[1, 2], [3, 4], [5, 6], [7, 8]]
   console.log('Test 3:', chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
+
+
+ function frequency(string) {
+    var frequen = {};
+    for (var i=0; i<string.length;i++) {
+        var character = string.charAt(i);
+        if ( frequen[character]) {
+            frequen[character]++;
+        } 
+        else {
+            frequen[character] = 1;
+        }
+    }
+    return  frequen;
+   }
+   
+   console.log('Test 1:', frequency('cccbbbaaa'))
+   // {a: 3, b: 3, c: 3}
+   console.log('Test 2:', frequency('www.bedu.org'))
+   // {.: 2, b: 1, d: 1, e: 1, g: 1, o: 1, r: 1, u: 1, w: 3}
+   console.log('Test 3:', frequency('john.doe@domain.com'))
+   // {.: 2, @: 1, a: 1, c: 1, d: 2, e: 1, h: 1, i: 1, j: 1, m: 2, n: 2, o: 4}
