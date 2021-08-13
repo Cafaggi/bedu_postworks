471c8c76709af645e084c7a495dd86f86b680f6c
cafaggi <carlos.cafaggi@gmail.com>
2021-08-13 17:34:01 -0500

    Set the html into the directory for babel
    move the elements on the correct path for transpiling

--- project/src/js/index.js
+++ project/src/js/index.js
@@ -1,8 +1,1 @@
 import '../css/styles.css';
-import mustang1 from '../assets/mustang1.jpg';
-import mustang2 from '../assets/mustang2.jpg';
-import mustang3 from '../assets/mustang1.jpg';
-
-document.getElementById('mustang1').src = mustang1
-document.getElementById('mustang2').src = mustang2
-document.getElementById('mustang3').src = mustang3
