a7d5807cbef5bf11dac4454afe243ddfc7ee076f
cafaggi <carlos.cafaggi@gmail.com>
2021-08-16 20:27:01 -0500

    add functionality to searchbar

--- project/src/js/index.js
+++ project/src/js/index.js
@@ -1,1 +1,52 @@
 import '../css/styles.css';
+import 'regenerator-runtime/runtime';
+
+
+function createCard(data) {
+  const card = document.createElement('div');
+  card.className = 'meal-card';
+
+  const name = document.createElement('h2');
+  const nameText = document.createTextNode(data.strMeal);
+  name.appendChild(nameText)
+
+  const instructions = document.createElement('h6');
+  const instText = document.createTextNode(data.strInstructions);
+  instructions.appendChild(instText)
+
+  const img = document.createElement('img');
+  img.src = data.strMealThumb;
+  img.alt = data.strMeal;
+  img.width = 150
+  img.height = 75
+
+  card.appendChild(name);
+  card.appendChild(img);
+  card.appendChild(instructions);
+
+  return card;
+}
+
+
+
+var searchButton = document.getElementById("search-button")
+const searchInput = document.getElementById('search-input-box');
+
+searchButton.addEventListener('click', getSearchedMeal);
+
+async function getSearchedMeal() {
+  const API_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+  const search = searchInput.value;
+
+  if(search) {
+    const formattedSearch = API_URL+String(search);
+    const response = await fetch(formattedSearch);
+    let data = await response.json()
+
+    data = data.meals[0];
+    const card = document.querySelector('#searched-meal')
+    card.appendChild(createCard(data))
+  }
+  
+}
+
